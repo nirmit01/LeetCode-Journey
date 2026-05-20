e008447461b98f0caad8a324f6868311c4e6f419
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-05-21 04:04:27 +0530

    Time: 0 ms (100%), Space: 8.3 MB (10.2%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int ans=0;
+        while(n>0)
+        {
+            if(n%2!=0)
+                ans++;
+            n/=2;
+        }
+        return ans;
+    }
+};
