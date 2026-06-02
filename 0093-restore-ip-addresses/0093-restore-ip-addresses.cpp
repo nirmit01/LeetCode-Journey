c38c8e1e25bfa9d9276d02c2ae677bc07f3d3d5c
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-06-02 20:45:33 +0530

    Time: 3 ms (31.1%), Space: 10.5 MB (73.39%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    void solve(int i,string& s,int cnt,vector<string>& ans,string temp)
+    {
+        if(cnt==4 && i==(int)s.size())
+        {
+            ans.push_back(temp);
+            return;
+        }
+        if(i==(int)s.size())  return;
+        if(cnt==4)  return;
+        if(cnt>0 && cnt<=3)
+            temp+='.';
+        string ch=temp;
+        if(s[i]=='0')
+        {
+            temp+='0';
+            solve(i+1,s,cnt+1,ans,temp);
+            return;
+        }
+        int n=s.size();
+        string dig="";
+        for(int j=i;j<i+3 && j<n;j++)
+        {
+            dig+=s[j];
+            int d=0;
+            for(int k=0;k<dig.size();k++)
+            {
+                d=(d*10)+(dig[k]-'0');
+            }
+            if(d<=255)
+            {
+                ch=temp+dig;
+                solve(j+1,s,cnt+1,ans,ch);
+            }
+        }
+    }
+
+    vector<string> restoreIpAddresses(string s) {
+        string temp="";
+        vector<string> ans;
+        solve(0,s,0,ans,temp);
+        return ans;
+    }
+};
