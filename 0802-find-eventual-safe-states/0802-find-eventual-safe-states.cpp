18f6a68c532c2df036bece42513bcb1bdeee5e40
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-06-06 19:52:48 +0530

    Time: 4 ms (89.4%), Space: 51.9 MB (87.56%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
+
+    bool dfs(int i, vector<int>& state, vector<vector<int>>& graph)
+    {
+        if(state[i]==2) return true;
+        if(state[i]==1) return false;
+
+        state[i]=1;
+        for(auto& x: graph[i])
+        {
+            if(!dfs(x,state,graph))
+            {
+                return false;
+            }
+        }
+        state[i]=2;
+        return true;
+    }
+
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n=graph.size();
-        vector<vector<int>> adj(n);
-        vector<int> indegree(n);
+        vector<int> state(n),ans;
         for(int i=0;i<n;i++)
         {
-            indegree[i]+=graph[i].size();
-            for(auto&v : graph[i])
-            {
-                adj[v].push_back(i);
-            }
+            if(dfs(i,state,graph))
+                ans.push_back(i);
         }
-        queue<int> q;
-        for(int i=0;i<n;i++)
-        {
-            if(indegree[i]==0)
-                q.push(i);
-        }
-        vector<int> ans;
-        while(!q.empty())
-        {
-            int k=q.front();
-            q.pop();
-            ans.push_back(k);
-            for(auto& x : adj[k])
-            {
-                indegree[x]--;
-                if(indegree[x]==0)
-                    q.push(x);
-            }
-        }
-        sort(ans.begin(),ans.end());
         return ans;
     }
 };
