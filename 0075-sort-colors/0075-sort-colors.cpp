518470a82f5bf9110985761f58468d7c2974051d
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-06-08 14:50:39 +0530

    Time: 0 ms (100%), Space: 11.6 MB (77.61%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        int i=0,j=n-1,mid=0;
+        while(mid<=j)
+        {
+            if(nums[mid]==0)
+            {
+                swap(nums[mid],nums[i]);
+                i++;
+                mid++;
+            }
+            else if(nums[mid]==2)
+            {
+                swap(nums[mid],nums[j]);
+                j--;
+            }
+            else
+            {
+                mid++;
+            }
+        }
+    }
+};
