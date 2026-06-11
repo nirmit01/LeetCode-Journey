54d67a73acef47a4e4f4cd43dae219e6464cddbb
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-06-11 23:57:55 +0530

    Time: 9 ms (24.37%), Space: 18.3 MB (20.64%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    void dfs(int node,int target,vector<int>temp, vector<vector<int>>& adj,vector<vector<int>>& ans,vector<int> vis)
+    int target;
+    void dfs(int node,vector<int>temp, vector<vector<int>>& adj,vector<vector<int>>& ans)
     {
-        if(target==node)
+        if(node==target)
         {
-            temp.push_back(target);
             ans.push_back(temp);
+            return;
         }
-        vis[node]=1;
-        temp.push_back(node);
         for(int x:adj[node])
         {
-            dfs(x,target,temp,adj,ans,vis);
+            if(find(temp.begin(),temp.end(),x)==temp.end())
+            {
+                temp.push_back(x);
+                dfs(x,temp,adj,ans);
+                temp.pop_back();
+            }
         }
     }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         vector<vector<int>> ans;
         vector<int> temp;
         int n=graph.size();
-        vector<int> vis(n);
-        dfs(0,n-1,temp,graph,ans,vis);
+        target=n-1;
+        temp.push_back(0);
+        dfs(0,temp,graph,ans);
         return ans;
     }
 };
