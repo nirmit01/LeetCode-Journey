d692efacc7fcbafddc74f47e426970affe2278f7
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-06-13 03:35:17 +0530

    Time: 0 ms (100%), Space: 7.9 MB (1.57%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        int carry;
+        while(b!=0)
+        {
+            carry=(a&b)<<1;
+            a=a^b;
+            b=carry;
+        }
+        return a;
+    }
+};
