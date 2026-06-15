dbbebf937fbf25970763ffbea254bfc07368b8b3
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-06-15 09:47:57 +0530

    Time: 0 ms (100%), Space: 17.1 MB (65.2%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -2,33 +2,34 @@ class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
         stack<int> st;
-        for(string s:tokens)
+        for(string c:tokens)
         {
-            if((s[0]>=48 && s[0]<=57) || (s[0]=='-' && s.size()>1))
+            if(c=="+")
             {
-                int num=0,k=1;
-                if(s[0]=='-')
-                    k=-1;
-                for(char c:s)
-                {
-                    if(c=='-')
-                        continue;
-                    else
-                        num=num*10+(c-'0');
-                }
-                st.push(k*num);
+                int a=st.top();st.pop();
+                int b=st.top();st.pop();
+                st.push(a+b);
+            }
+            else if(c=="-")
+            {
+                int a=st.top();st.pop();
+                int b=st.top();st.pop();
+                st.push(b-a);
+            }
+            else if(c=="*")
+            {
+                int a=st.top();st.pop();
+                int b=st.top();st.pop();
+                st.push(a*b);
+            }
+            else if(c=="/")
+            {
+                int a=st.top();st.pop();
+                int b=st.top();st.pop();
+                st.push(b/a);
             }
             else
-            {
-                int x=st.top();
-                st.pop();
-                int y=st.top();
-                st.pop();
-                if(s[0]=='/')   st.push(y/x);
-                else if(s[0]=='*')  st.push(y*x);
-                else if(s[0]=='+')  st.push(x+y);
-                else    st.push(y-x);
-            }
+                st.push(stoi(c));
         }
         return st.top();
     }
