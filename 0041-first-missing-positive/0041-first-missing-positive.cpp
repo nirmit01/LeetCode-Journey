c8958670b0eaac3481d26d603881eb0a49841219
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-06-21 23:34:14 +0530

    Time: 0 ms (100%), Space: 55.1 MB (31.05%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -4,21 +4,12 @@ public:
         int n=nums.size();
         for(int i=0;i<n;i++)
         {
-            if(nums[i]<=0 || nums[i]>n)
-                nums[i]=n+1;
+            while(nums[i]>0 && nums[i]<=n && nums[i]!=nums[nums[i]-1])
+                swap(nums[i],nums[nums[i]-1]);
         }
         for(int i=0;i<n;i++)
         {
-            int k=abs(nums[i]);
-            if(k<=n)
-            {
-                if(nums[k-1]>0)
-                    nums[k-1]=-nums[k-1];
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]>0)
+            if(nums[i]!=i+1)
                 return i+1;
         }
         return n+1;
