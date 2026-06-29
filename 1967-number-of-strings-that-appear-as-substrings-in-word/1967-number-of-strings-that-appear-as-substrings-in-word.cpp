0212161317a13fe78a21fdcf7a1223c8cb7bfd10
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-06-29 19:28:08 +0530

    Time: 0 ms (100%), Space: 11.6 MB (61.65%) - LeetHub

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     int numOfStrings(vector<string>& patterns, string word) {
         int cnt=0;
+        int n=word.size();
         for(string s:patterns)
-        {
             if(word.find(s)!=string::npos)
                 cnt++;
-        }
         return cnt;
     }
 };
