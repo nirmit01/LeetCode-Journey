3a9eeb60a293142dd2317854575ce36d5b1bb320
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-07-06 16:12:21 +0530

    Time: 11 ms (93.34%), Space: 41.7 MB (78.89%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     int mostFrequentEven(vector<int>& nums) {
-        int n=nums.size(),maxi=0;
-        bool flag=false;
         unordered_map<int,int> mpp;
         for(int i:nums)
         {
             if(i%2==0)
-            {
                 mpp[i]++;
-                flag=true;
-                maxi=max(maxi,mpp[i]);
-            }
         }
-        if(!flag)
-            return -1;
-        int ans=INT_MAX;
-        for(auto k:mpp)
+        int ans=-1,maxi=0;
+        for(auto [x,freq]:mpp)
         {
-            if(k.second==maxi)
-                ans=min(ans,k.first);
+            if(freq>maxi || (freq==maxi && x<ans))
+            {
+                maxi=freq;
+                ans=x;
+            }
         }
         return ans;
     }
