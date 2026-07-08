fbb952ba6f2d5ba8baf042a8c2b04bbf7c9f4d21
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-07-09 04:10:51 +0530

    Time: 79 ms (33.97%), Space: 158.8 MB (45.37%) - LeetHub

--- 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -1,18 +1,13 @@
+long long pow10[100001];
+const int mod=1e9+7;
+int init=[](){
+    pow10[0]=1;
+    for(int i=1;i<100001;i++)
+        pow10[i]=(pow10[i-1]*10)%mod;
+    return 0;
+}();
 class Solution {
 public:
-    const int mod=1e9+7;
-    int binexp(int e,int x)
-    {
-        long long ans=1,a=e;
-        while(x>0)
-        {
-            if(x&1)
-                ans=(ans*a)%mod;
-            a=(a*a)%mod;
-            x>>=1;
-        }
-        return ans%mod;
-    }
     vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
         vector<int> dig,pos;
         int n=s.size();
@@ -38,7 +33,7 @@ public:
             int l=lower_bound(pos.begin(),pos.end(),v[0])-pos.begin();
             int r=upper_bound(pos.begin(),pos.end(),v[1])-pos.begin();
             int cur=pref[r]-pref[l];
-            long long x=(conc[r]-((conc[l])*binexp(10,r-l))%mod + mod)%mod;
+            long long x=(conc[r]-(conc[l]*pow10[r-l])%mod + mod)%mod;
             ans.push_back((x*cur)%mod);
         }
         return ans;
