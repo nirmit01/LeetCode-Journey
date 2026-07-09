d527397fdae89636a192261477bb128c55a3881b
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-07-10 00:54:52 +0530

    Time: 11 ms (65.23%), Space: 43.9 MB (64.73%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> parent,size;
+    int find(int x)
+    {
+        if(parent[x]==x)
+            return x;
+        return parent[x]=find(parent[x]);
+    }
+
+    void unite(int x,int y)
+    {
+        int px=find(x);
+        int py=find(y);
+        if(px==py)
+            return;
+        if(size[px]<size[py])
+            swap(px,py);
+        parent[py]=px;
+        size[px]+=size[py];
+    }
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        int m=connections.size();
+        if(m<n-1)
+            return -1;
+        parent=vector<int>(n);
+        size=vector<int>(n,1);
+        for(int i=0;i<n;i++)
+        {
+            parent[i]=i;
+        }
+        for(auto& v:connections)
+        {
+            unite(v[0],v[1]);
+        }
+        unordered_set<int> st;
+        for(int i:parent)
+            st.insert(find(i));
+        return st.size()-1;
+    }
+};
