2bedf9cb1758126f33d7c3b43d97267e6eff34d6
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-07-09 23:36:14 +0530

    Time: 11 ms (73.15%), Space: 223.9 MB (90.41%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -1,38 +1,20 @@
 class Solution {
 public:
-
-    int find(int x,vector<int>& parent)
-    {
-        if(parent[x]==x)
-            return x;
-        return parent[x]=find(parent[x],parent);
-    }
-
-    void merge(int x, int y,vector<int>& parent, vector<int>& size)
-    {
-        int px=find(x,parent);
-        int py=find(y,parent);
-        if(px==py)
-            return;
-        if(size[px]>size[py])
-            swap(px,py);
-        parent[py]=px;
-        size[px]+=size[py];
-    }
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        vector<int> parent(n),size(n,1);
-        for(int i=0;i<n;i++)
-            parent[i]=i;
+        vector<int> group(n);
+        int id=0;
         for(int i=1;i<n;i++)
         {
-            if(abs(nums[i]-nums[i-1])<=maxDiff)
-                merge(i,i-1,parent,size);
+            if((nums[i]-nums[i-1])>maxDiff)
+            {
+                id++;
+            }
+            group[i]=id; 
         }
-
         vector<bool> ans;
         for(auto& v:queries)
         {
-            ans.push_back(find(v[0],parent)==find(v[1],parent));
+            ans.push_back(group[v[0]]==group[v[1]]);
         }
         return ans;
     }
