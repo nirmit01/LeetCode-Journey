f07ca1cfefccabc0c84d11709e061be60f175357
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-07-18 04:29:37 +0530

    Time: 75 ms (14.35%), Space: 50.9 MB (94.53%) - LeetHub

---
+++ 1705-maximum-number-of-eaten-apples/1705-maximum-number-of-eaten-apples.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int eatenApples(vector<int>& apples, vector<int>& days) {
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>> pq;
+        int n=apples.size();
+        int cnt=0, i=0;
+        while(i<n || !pq.empty())
+        {
+            if(i<n)
+                pq.push({i+days[i],apples[i]});
+            while(!pq.empty() && pq.top().first<=i)
+            {
+                pq.pop();
+            }
+            if(!pq.empty())
+            {
+                auto k=pq.top();
+                pq.pop();
+                cnt++;
+                k.second--;
+                if(k.second>0)
+                    pq.push({k.first,k.second});
+            }
+            i++;
+        }
+        return cnt;
+    }
+};
