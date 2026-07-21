5a582af4f3b010c7fe2950446971dd2f69d371b5
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-07-21 11:43:55 +0530

    Time: 44 ms (86.49%), Space: 54.9 MB (76.13%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -2,41 +2,27 @@ class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
         int i=0,n=s.size();
-        vector<pair<int,int>> vec;
+        int ones=0,prev=0,ans=0;
         while(i<n)
         {
+            if(i<n && s[i]=='1')
+            {
+                ones++;
+                i++;
+                continue;
+            }
             int cnt=0;
-            while(s[i]=='1')
+            while(i<n  && s[i]=='0')
             {
                 cnt++;
                 i++;
             }
-            if(cnt>0)
-                vec.push_back({cnt,1});
-            cnt=0;
-            while(s[i]=='0')
+            if(prev>0)
             {
-                cnt++;
-                i++;
+                ans=max(ans,prev+cnt);
             }
-            if(cnt>0)
-                vec.push_back({cnt,0});
-        }
-        int sz=vec.size();
-        int ans=0;
-        for(int i=0;i<n;i++)
-        {
-            if(s[i]=='1')
-                ans++;
-        }
-        int maxi=0;
-        for(int i=1;i<sz-1;i++)
-        {
-            if(vec[i].second==1 && vec[i-1].second==0 && vec[i+1].second==0)
-            {
-                maxi=max(maxi,vec[i-1].first+vec[i+1].first);
-            }
-        }
-        return ans+maxi;
+            prev=cnt;
+        }   
+        return ans+ones;
     }
 };
