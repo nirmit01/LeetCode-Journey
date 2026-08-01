e453118a87b2185b73118691e91afc3e070aefc2
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-08-01 06:03:21 +0530

    Time: 0 ms (100%), Space: 10.3 MB (32.54%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -5,7 +5,7 @@ public:
     {
         if(l==r)
             return dp[l][r]=nums[l];
-        if(dp[l][r]!=-1)
+        if(dp[l][r]!=INT_MIN)
             return dp[l][r];
         
         return dp[l][r]=max(nums[l]-solve(l+1,r,nums,dp),nums[r]-solve(l,r-1,nums,dp));
@@ -14,7 +14,7 @@ public:
 
     bool predictTheWinner(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
+        vector<vector<int>> dp(n,vector<int>(n,INT_MIN));
         return solve(0,n-1,nums,dp)>=0;
     }
 };
