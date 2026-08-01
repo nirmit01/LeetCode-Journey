3d176c834c41efda49af99cb2370ec2c71f40c70
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-08-01 05:59:19 +0530

    Time: 19 ms (14.14%), Space: 19.8 MB (11.11%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int solve(int l,int r, vector<int>& nums,vector<vector<int>>& dp)
+    {
+        if(l==r)
+            return dp[l][r]=nums[l];
+        if(dp[l][r]!=-1)
+            return dp[l][r];
+        
+        return dp[l][r]=max(nums[l]-solve(l+1,r,nums,dp),nums[r]-solve(l,r-1,nums,dp));
+
+    }
+
+    bool stoneGame(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return solve(0,n-1,nums,dp)>=0;
+    }
+};
