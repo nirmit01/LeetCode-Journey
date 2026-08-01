4d3436a5d461be880237ad5e382874b92f17d301
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-08-01 06:34:40 +0530

    Time: 130 ms (56.08%), Space: 275.8 MB (5%) - LeetHub

--- 0464-can-i-win/0464-can-i-win.cpp
+++ 0464-can-i-win/0464-can-i-win.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    unordered_map<int,bool> mpp;
+    vector<int> dp;
     bool solve(int mask, int rem,int n)
     {
-        if(mpp.count(mask))
-            return mpp[mask];
+        if(dp[mask]!=-1)
+            return dp[mask];
         
         for(int i=1;i<=n;i++)
         {
@@ -12,16 +12,17 @@ public:
                 continue;
             
             if(i>=rem)
-                return mpp[mask]=true;
+                return dp[mask]=true;
             
             int newmask= mask | 1<<(i-1);
             if(!solve(newmask,rem-i,n))
-                return mpp[mask]=true;
+                return dp[mask]=true;
         }
-        return mpp[mask]=false;
+        return dp[mask]=false;
     }
 
     bool canIWin(int maxChoosableInteger, int desiredTotal) {
+        dp=vector<int>(1<<20,-1);
         int tot= (maxChoosableInteger*(maxChoosableInteger+1))/2;
         if(desiredTotal<=maxChoosableInteger)
             return true;
