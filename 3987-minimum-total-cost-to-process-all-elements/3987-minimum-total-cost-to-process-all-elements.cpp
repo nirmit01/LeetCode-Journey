16de3497eac14525f55f32390ca2c13e584146b0
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-08-03 18:49:57 +0530

    Time: 0 ms (100%), Space: 169.4 MB (64.95%) - LeetHub

--- 3987-minimum-total-cost-to-process-all-elements/3987-minimum-total-cost-to-process-all-elements.cpp
+++ 3987-minimum-total-cost-to-process-all-elements/3987-minimum-total-cost-to-process-all-elements.cpp
@@ -3,21 +3,11 @@ public:
     const int mod=1e9+7;
     const int inv2=500000004;
     int minimumCost(vector<int>& nums, int k) {
-        long long cost=0,curr=k;
+        long long summ=0;
         for(int x:nums)
-        {
-            if(curr<x)
-            {
-                int diff=x-curr;
-                int req= (diff%k) ? diff/k+1 : diff/k;
-                cost+=req;
-                curr= (curr+req*k)-x;
-            }
-            else
-            {
-                curr-=x;
-            }
-        }
+            summ+=x;
+        long long cost= (summ-1)/k;
+
         return (cost%mod)*((cost+1)%mod)%mod*inv2%mod;
     }
 };
