1b5de04209840bd62b55c2e5de76ce27996f58e2
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-08-05 03:11:14 +0530

    Time: 0 ms (100%), Space: 7.7 MB (58.17%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,22 +1,9 @@
 class Solution {
 public:
-    bool solve(int n,vector<int>& dp)
-    {
-        if(n==1)
-            return false;
-        if(dp[n]!=-1)
-            return dp[n];
-        
-        for(int x=1;x<n;x++)
-            if(n%x==0)
-                if(!solve(n-x,dp))
-                    return dp[n]=true;
-        
-        return dp[n]=false;
-
-    }
     bool divisorGame(int n) {
-        vector<int> dp(n+1,-1);
-        return solve(n,dp);
+        if(n%2)
+            return false;
+        else
+            return true;   
     }
 };
