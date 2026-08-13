87d4c7690a1290b8480adc6877b56564a6a9fcdd
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-08-13 19:11:49 +0530

    Time: 0 ms (100%), Space: 9.1 MB (66.26%) - LeetHub

---
+++ 2957-remove-adjacent-almost-equal-characters/2957-remove-adjacent-almost-equal-characters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeAlmostEqualCharacters(string word) {
+        int n=word.size();
+        int ans=0;
+        for(int i=0;i<n-1;i++)
+        {
+            if(abs(word[i]-word[i+1])<=1)
+            {
+                ans++;
+                i++;
+            }
+        }
+        return ans;
+    }
+};
