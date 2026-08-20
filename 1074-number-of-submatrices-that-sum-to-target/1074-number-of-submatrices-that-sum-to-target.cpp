8458edb5f26e427bf14f29d922a5ce3921f10931
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-08-21 01:50:27 +0530

    Time: 435 ms (79.9%), Space: 154.7 MB (65.19%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
+    Solution()
+    {
+        cin.tie(NULL);
+        cout.tie(NULL);
+        ios_base :: sync_with_stdio(false);
+    }
     int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
         int n=matrix.size(), m=matrix[0].size();
-        int ans=0;
-        for(int top=0;top<n;top++)
+        for(int i=0;i<n;i++)
         {
-            vector<int> pref(m);
-            for(int bottom=top;bottom<n;bottom++)
+            for(int j=1;j<m;j++)
             {
-                for(int i=0;i<m;i++)
-                    pref[i]+=matrix[bottom][i];
-                
+                matrix[i][j]+=matrix[i][j-1];
+            }
+        }
+        int ans=0;
+        for(int left=0;left<m;left++)
+        {
+            for(int right=left;right<m;right++)
+            {
                 unordered_map<int,int> freq;
                 freq[0]=1;
                 int tot=0;
-                for(int i=0;i<m;i++)
+                for(int i=0;i<n;i++)
                 {
-                    tot+=pref[i];
-                    if(freq[tot-target])
-                        ans+=freq[tot-target];
+                    if(left>0)
+                        tot+=matrix[i][right]-matrix[i][left-1];
+                    else
+                        tot+=matrix[i][right];
+                    auto it=freq.find(tot-target);
+                    if(it!=freq.end())
+                        ans+=it->second;
                     freq[tot]++;
                 }
             }
