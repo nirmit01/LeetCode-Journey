af14ec6c381568133a786ff0bd0a5858c5d3f738
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-08-22 19:57:29 +0530

    Time: 2 ms (64.69%), Space: 8.2 MB (97.68%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -2,27 +2,23 @@ class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
         int n=s1.size(), m=s2.size();
-        if(s3.size()!=n+m){
+        if(s3.size()!=n+m)
             return false;
-        }
-        vector<vector<bool>> dp(n+1,vector<bool>(m+1));
-        dp[0][0]=true;
-        for(int i=1;i<=n;i++){
-            dp[i][0]=dp[i-1][0] && s1[i-1]==s3[i-1];
-        }
-        for(int j=1;j<=m;j++){
-            dp[0][j]=dp[0][j-1] && s2[j-1]==s3[j-1];
-        }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s1[i-1]==s3[i+j-1]){
-                    dp[i][j]=dp[i][j] || dp[i-1][j];
-                }
-                if(s2[j-1]==s3[i+j-1]){
-                    dp[i][j]=dp[i][j] || dp[i][j-1];
-                }
+
+        vector<bool> dp(m+1);
+        dp[0]=true;
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0 && j==0)
+                    continue;
+                bool check1=false, check2=false;
+                if(i>0 && s1[i-1]==s3[i+j-1])
+                    check1=dp[j];
+                if(j>0 && s2[j-1]==s3[i+j-1])
+                    check2=dp[j-1];
+                dp[j]=check1 || check2;
             }
         }
-        return dp[n][m];
+        return dp[m];
     }
 };
