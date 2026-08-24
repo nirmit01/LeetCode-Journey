af8022df4014b9a194a73244d8c49012d39385e5
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-08-24 21:10:07 +0530

    Time: 0 ms (100%), Space: 9.3 MB (23.23%) - LeetHub

--- 0999-available-captures-for-rook/0999-available-captures-for-rook.cpp
+++ 0999-available-captures-for-rook/0999-available-captures-for-rook.cpp
@@ -14,55 +14,24 @@ public:
             }
         }
         int ans=0;
-        int i1=x-1,i2=x+1;
-        int j1=y-1,j2=y+1;
-        while(i1>=0)
+        int dx[]={-1,0,1,0};
+        int dy[]={0,-1,0,1};
+        for(int d=0;d<4;d++)
         {
-            if(board[i1][y]=='p')
+            int i=x+dx[d];
+            int j=y+dy[d];
+            while(i>=0 && i<8 && j>=0 && j<8)
             {
-                ans++;
-                break;
+                if(board[i][j]=='p')
+                {
+                    ans++;
+                    break;
+                }
+                else if(board[i][j]=='B')
+                    break;
+                i+=dx[d];
+                j+=dy[d];
             }
-            else if(board[i1][y]=='B')
-                break;
-            else
-                i1--;
-        }
-        while(i2<n)
-        {
-            if(board[i2][y]=='p')
-            {
-                ans++;
-                break;
-            }
-            else if(board[i2][y]=='B')
-                break;
-            else
-                i2++;
-        }
-        while(j2<n)
-        {
-            if(board[x][j2]=='p')
-            {
-                ans++;
-                break;
-            }
-            else if(board[x][j2]=='B')
-                break;
-            else
-                j2++;
-        }
-        while(j1>=0)
-        {
-            if(board[x][j1]=='p')
-            {
-                ans++;
-                break;
-            }
-            else if(board[x][j1]=='B')
-                break;
-            else
-                j1--;
         }
         return ans;
     }
