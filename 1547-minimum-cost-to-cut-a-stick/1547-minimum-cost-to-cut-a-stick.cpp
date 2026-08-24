251816edd6d221ee3a7077be7712bf93ed81e33a
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-08-25 01:55:39 +0530

    Time: 19 ms (81.68%), Space: 13.9 MB (54.64%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -6,13 +6,10 @@ public:
         cuts.push_back(n);
         sort(cuts.begin(),cuts.end());
         vector<vector<int>> dp(sz+2,vector<int>(sz+2));
-        for(int i=sz;i>=1;i--)
-        {
-            for(int j=i;j<=sz;j++)
-            {
+        for(int i=sz;i>=1;i--){
+            for(int j=i;j<=sz;j++){
                 int mini=INT_MAX;
-                for(int k=i;k<=j;k++)
-                {
+                for(int k=i;k<=j;k++){
                     mini=min(mini, cuts[j+1]-cuts[i-1]+dp[i][k-1]+dp[k+1][j]);
                 }
                 dp[i][j]=mini;
