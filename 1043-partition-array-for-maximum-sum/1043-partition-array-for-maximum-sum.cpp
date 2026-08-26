41f361e5db67ec83347d62367f84642511d0aadd
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-08-26 22:17:05 +0530

    Time: 7 ms (51.59%), Space: 12.6 MB (30.85%) - LeetHub

---
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        int n=arr.size();
+        vector<int> dp(n+1);
+        for(int i=1;i<=n;i++)
+        {
+            int maxi=-1;
+            for(int j=1;j<=k && (i-j)>=0;j++)
+            {
+                maxi=max(maxi, arr[i-j]);
+                dp[i]=max(dp[i],dp[i-j]+maxi*j);
+            }
+        }
+        return dp[n];
+    }
+};
