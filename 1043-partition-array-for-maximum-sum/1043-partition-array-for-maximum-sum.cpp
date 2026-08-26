4f3463bfc49d8a92df96d0655238eab329676787
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-08-26 22:18:52 +0530

    Time: 5 ms (62.04%), Space: 12.4 MB (87.5%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -6,11 +6,13 @@ public:
         for(int i=1;i<=n;i++)
         {
             int maxi=-1;
+            int best=0;
             for(int j=1;j<=k && (i-j)>=0;j++)
             {
                 maxi=max(maxi, arr[i-j]);
-                dp[i]=max(dp[i],dp[i-j]+maxi*j);
+                best=max(best,dp[i-j]+maxi*j);
             }
+            dp[i]=best;
         }
         return dp[n];
     }
