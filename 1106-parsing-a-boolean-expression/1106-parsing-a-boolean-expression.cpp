b6a41acf338b71ec9d7f5277c74cd966fe0bc0b2
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-09-01 00:55:25 +0530

    Time: 2 ms (62.74%), Space: 10.9 MB (69.81%) - LeetHub

--- 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.cpp
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.cpp
@@ -2,35 +2,28 @@ class Solution {
 public:
     bool parseBoolExpr(string expression) {
         stack<char> st;
-        int n=expression.size();
         for(char ch:expression)
         {
-            if(ch==',' || ch=='(')
+            if(ch==',' || ch=='('){
                 continue;
-            else if(ch!=')')
+            }
+            else if(ch!=')'){
                 st.push(ch);
-            else
-            {
+            }
+            else{
                 bool hastrue=false, hasfalse=false;
-                while(st.top()=='f' || st.top()=='t')
-                {
+                while(st.top()=='f' || st.top()=='t'){
                     char c=st.top();
                     st.pop();
-                    if(c=='t')
-                        hastrue=true;
-                    else 
-                        hasfalse=true;
+                    if(c=='t')  hastrue=true;
+                    else    hasfalse=true;
                 }
                 char op=st.top();
                 st.pop();
                 bool ans;
-                if(op=='!')
-                    ans=hasfalse;
-                else if(op=='&')
-                    ans=!hasfalse;
-                else
-                    ans=hastrue;
-                
+                if(op=='!') ans=hasfalse;
+                else if(op=='&')    ans=!hasfalse;
+                else    ans=hastrue;
                 st.push(ans?'t':'f');
             }
         }
