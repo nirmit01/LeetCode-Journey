3b1cc977e6f67394a7a59d612f77f2ebfd779e01
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-09-02 17:25:51 +0530

    Time: 0 ms (100%), Space: 11.9 MB (18.05%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -2,17 +2,12 @@ class Solution {
 public:
     vector<string> fizzBuzz(int n) {
         vector<string> ans;
-        for(int i=1;i<=n;i++)
-        {
+        for(int i=1;i<=n;i++){
             int k1=i%3, k2=i%5;
-            if(k1==0 && k2==0)
-                ans.push_back("FizzBuzz");
-            else if(k1==0)
-                ans.push_back("Fizz");
-            else if(k2==0)
-                ans.push_back("Buzz");
-            else
-                ans.push_back(to_string(i));
+            if(k1==0 && k2==0)  ans.push_back("FizzBuzz");
+            else if(k1==0)  ans.push_back("Fizz");
+            else if(k2==0)  ans.push_back("Buzz");
+            else    ans.push_back(to_string(i));
         }
         return ans;
     }
