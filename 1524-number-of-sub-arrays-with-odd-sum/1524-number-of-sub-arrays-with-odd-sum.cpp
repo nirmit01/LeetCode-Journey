835f8f276e8bf28e8c3c552340c239109b327400
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-09-02 19:33:01 +0530

    Time: 6 ms (44.05%), Space: 112 MB (51.73%) - LeetHub

--- 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
-    const int mod=1e9+7;
     int numOfSubarrays(vector<int>& arr) {
-        long long eve=1, odd=0;
-        int n=arr.size(), ans=0;
-        int pref=0;
+        int mod=1e9+7;
+        int eve=1, odd=0;
+        int n=arr.size(), ans=0, pref=0;
         for(int i=0;i<n;i++){
             pref+=arr[i];
             if(pref%2){
