1d57f4f3ad8326ed3e2dc9c136c8eb2672323c30
Nirmit Chaurasia <chaurasianirmit@gmail.com>
2026-09-03 11:52:56 +0530

    Time: 8 ms (80.76%), Space: 58 MB (93.13%) - LeetHub

--- 3148-maximum-difference-score-in-a-grid/3148-maximum-difference-score-in-a-grid.cpp
+++ 3148-maximum-difference-score-in-a-grid/3148-maximum-difference-score-in-a-grid.cpp
@@ -3,26 +3,20 @@ public:
     int maxScore(vector<vector<int>>& grid) {
         int n=grid.size(), m=grid[0].size();
         int ans=INT_MIN;
-        for(int i=1;i<n;i++)
-        {
-            int k=grid[i][0];
-            ans=max(ans, k-grid[i-1][0]);
-            grid[i][0]=min(grid[i][0],grid[i-1][0]);
-        }
-        for(int j=1;j<m;j++)
-        {
-            int k=grid[0][j];
-            ans=max(ans, k-grid[0][j-1]);
-            grid[0][j]=min(grid[0][j],grid[0][j-1]);
-        }
-        for(int i=1;i<n;i++)
-        {
-            for(int j=1;j<m;j++)
-            {
-                int k=grid[i][j];
-                int mn=min(grid[i][j-1],grid[i-1][j]);
-                ans=max(ans,k-mn);
-                grid[i][j]=min(grid[i][j],mn);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int curr=grid[i][j];
+                int mn=INT_MAX;
+                if(i>0){
+                    mn=min(mn,grid[i-1][j]);
+                }
+                if(j>0){
+                    mn=min(mn,grid[i][j-1]);
+                }
+                if(mn!=INT_MAX){
+                    ans=max(ans,curr-mn);
+                    grid[i][j]=min(mn,curr);
+                }
             }
         }
         return ans;
